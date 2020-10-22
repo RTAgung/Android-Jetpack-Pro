c14146f2b81e4532abd2373dd73944c3b30caa37
Rama Tri Agung <ramatriagung91@gmail.com>
2020-10-22 21:47:14 +0700

    View Model Architecture

---
+++ MyViewModel/app/src/main/java/com/example/myviewmodel/MainActivity.kt
@@ -0,0 +1,44 @@
+package com.example.myviewmodel
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.lifecycle.ViewModelProvider
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var viewModel: MainViewModel
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        viewModel = ViewModelProvider(this)[MainViewModel::class.java]
+
+        displayResult()
+
+        btn_calculate.setOnClickListener {
+            val width = edt_width.text.toString()
+            val height = edt_height.text.toString()
+            val length = edt_length.text.toString()
+            when {
+                width.isEmpty() -> {
+                    edt_width.error = "Masih kosong"
+                }
+                height.isEmpty() -> {
+                    edt_height.error = "Masih kosong"
+                }
+                length.isEmpty() -> {
+                    edt_length.error = "Masih kosong"
+                }
+                else -> {
+                    viewModel.calculate(width, height, length)
+                    displayResult()
+                }
+            }
+        }
+    }
+
+    private fun displayResult() {
+        tv_result.text = viewModel.result.toString()
+    }
+}
